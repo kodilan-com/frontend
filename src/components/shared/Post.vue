e1bb5d953b3d85703769fe11f385b8042af9f261
Fatih Acet <acetfatih@gmail.com>
2018-05-06 00:08:18 +0200

    Toggle post description.

--- src/components/shared/Post.vue
+++ src/components/shared/Post.vue
@@ -5,44 +5,68 @@ export default {
       type: Object,
       required: true,
     },
-  }
-}
+  },
+  data() {
+    return {
+      isExpanded: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.isExpanded = !this.isExpanded;
+    }
+  },
+};
 </script>
 
 <template>
-  <div class="tile tile-centered">
-    <div class="tile-icon">
-      <figure class="avatar avatar-lg">
-        <img
-          :src="post.company.logo"
-          :alt="post.company.name"
-        />
-      </figure>
-    </div>
-    <div class="tile-content">
-      <p class="tile-title">{{post.position}}</p>
-      <p class="tile-subtitle text-gray">
-        <a :href="`companies/${post.company.name}`">
-          {{post.company.name}}
+  <div>
+    <div
+      @click="toggle"
+      class="tile tile-centered"
+    >
+      <div class="tile-icon">
+        <figure class="avatar avatar-lg">
+          <img
+            :src="post.company.logo"
+            :alt="post.company.name"
+          />
+        </figure>
+      </div>
+      <div class="tile-content">
+        <p class="tile-title">{{post.position}}</p>
+        <p class="tile-subtitle text-gray">
+          <a :href="`companies/${post.company.name}`">
+            {{post.company.name}}
+          </a>
+        </p>
+        <span
+          v-for="tag in post.tags"
+          :key="tag.id"
+          class="label label-rounded label-secondary"
+        >
+          <a :href="`categories/${tag.slug}`">
+            {{tag.title}}
+          </a>
+        </span>
+      </div>
+      <div class="tile-action">
+        <a
+          :href="post.applyUrl"
+          class="btn btn-primary btn-sm"
+          target="_blank"
+        >
+          Başvur
         </a>
+      </div>
+    </div>
+    <div
+      v-if="isExpanded"
+      class="details"
+    >
+      <p>
+        {{post.description}}
       </p>
-      <span
-        v-for="tag in post.tags"
-        :key="tag.id"
-        class="label label-rounded label-secondary"
-      >
-        <a :href="`categories/${tag.slug}`">
-          {{tag.title}}
-        </a>
-      </span>
-    </div>
-    <div class="tile-action">
-      <a
-        :href="post.applyUrl"
-        class="btn btn-primary btn-sm"
-      >
-        Başvur
-      </a>
     </div>
   </div>
 </template>
