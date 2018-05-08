e2bb6d7ef645d408e6c18e24c2b814c85be4d41b
Fatih Acet <acetfatih@gmail.com>
2018-05-08 13:56:09 +0200

    Remove import of deleted deps.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import Vue from 'vue';
 import App from './App';
 import store from './store';
 import router from './router';
-import VeeValidate from 'vee-validate';
 
 Vue.config.productionTip = false;
-
-Vue.use(VeeValidate);
 
 /* eslint-disable no-new */
 new Vue({
