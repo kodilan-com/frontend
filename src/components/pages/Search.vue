34d8230ccc18462ec54dd1b1b0bed7c9558ba915
Fatih Acet <acetfatih@gmail.com>
2018-05-09 23:09:56 +0200

    Implement Search page.

--- src/components/pages/Search.vue
+++ src/components/pages/Search.vue
@@ -1,3 +1,68 @@
+<script>
+import { mapState, mapActions } from 'vuex';
+import JobListing from '../shared/JobListing';
+
+export default {
+  data() {
+    return {
+      isLoading: true,
+      posts: [],
+    };
+  },
+  components: {
+    JobListing,
+  },
+  computed: {
+    location() {
+      return this.$route.query.location;
+    },
+    query() {
+      return this.$route.query.query;
+    },
+  },
+  methods: {
+    ...mapActions(['search']),
+    handleSearch() {
+      this.search(this.$route.query)
+        .then((res) => {
+          this.isLoading = false;
+          this.posts = res.data;
+        })
+        .catch(() => {
+          this.isLoading = false;
+        });
+    },
+  },
+  created() {
+    this.handleSearch();
+  },
+};
+</script>
+
 <template>
-  <h1>{{$route.query}}</h1>
+  <div class="container">
+    <div
+      v-if="posts.length"
+      class="columns sixteen">
+      <h3 class="margin-bottom-25">
+        <template v-if="location">
+          <strong>{{location}}</strong> bölgesindeki
+        </template>
+        <template v-if="query">
+          <strong>{{query}}</strong> anahtar kelimesine sahip
+        </template>
+        <template v-if="location || query">ilanlar</template>
+      </h3>
+      <job-listing
+        :is-loading="isLoading"
+        :posts="posts"
+      />
+    </div>
+    <div
+      v-else
+      class="notification warning"
+    >
+      <p>Bu aramaya göre ilan bulamadık.</p>
+    </div>
+  </div>
 </template>
