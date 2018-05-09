6bc958385445cc1bfb19469e8ebae5fe98e0a79b
Fatih Acet <acetfatih@gmail.com>
2018-05-09 22:38:30 +0200

    Handle search in AppBanner.

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -1,12 +1,55 @@
+<script>
+export default {
+  data() {
+    return {
+      query: '',
+      location: '',
+    };
+  },
+  methods: {
+    handleSearch() {
+      const { query, location } = this;
+      const params = {};
+
+      if (!query && !location) {
+        return;
+      }
+
+      if (query) {
+        params.query = query;
+      }
+
+      if (location) {
+        params.location = location;
+      }
+
+      this.$router.push({ path: 'ilan-ara', query: params });
+    },
+  },
+};
+</script>
+
 <template>
-  <div id="banner" style="background-image: url(/static/images/banner.jpg)" class="parallax background" data-img-width="2000" data-img-height="1330" data-diff="400">
+  <div id="banner">
     <div class="container">
       <div class="sixteen columns">
         <div class="search-container">
           <h2>İlan Ara</h2>
-          <input type="text" class="ico-01" placeholder="pozisyon adı, şirket adı, teknoloji adı" value="" />
-          <input type="text" class="ico-02" placeholder="şehir" value="" />
-          <button>
+          <input
+            v-model="query"
+            @keypress.enter="handleSearch"
+            type="text"
+            class="ico-01"
+            placeholder="pozisyon adı, şirket adı, teknoloji adı"
+          />
+          <input
+            v-model="location"
+            @keypress.enter="handleSearch"
+            type="text"
+            class="ico-02"
+            placeholder="şehir"
+          />
+          <button @click="handleSearch">
             <i class="fa fa-search"></i>
           </button>
         </div>
