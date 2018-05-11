9fde6fabe7e04d58f20188a826c7ad4caa26c8eb
Fatih Acet <acetfatih@gmail.com>
2018-05-12 00:33:22 +0200

    Footer changes.

--- src/components/shared/AppFooter.vue
+++ src/components/shared/AppFooter.vue
@@ -1,26 +1,18 @@
 <template>
   <div id="footer">
     <div class="container">
-      <div class="sixteen columns">
-        <h4>About</h4>
-        <p>Morbi convallis bibendum urna ut viverra. Maecenas quis consequat libero, a feugiat eros. Nunc ut lacinia tortor
-          morbi ultricies laoreet ullamcorper phasellus semper.</p>
-      </div>
-    </div>
-
-    <!-- Bottom -->
-    <div class="container">
       <div class="footer-bottom">
         <div class="sixteen columns">
           <ul class="social-icons">
             <li>
-              <a class="twitter" href="#">
+              <a class="twitter" target="_blank" href="https://twitter.com/kodilancom">
                 <i class="icon-twitter"></i>
               </a>
             </li>
           </ul>
-          <div class="copyrights">© Copyright 2018 by
-            <a href="#">Work Scout</a>. All Rights Reserved.</div>
+          <div class="copyrights">© Copyright {{new Date().getFullYear()}} by
+            <router-link to="/">kodilan.com</router-link>.
+          </div>
         </div>
       </div>
     </div>
