1684972cc332675fe56c945535f3309f8379fc0f
Fatih Acet <acetfatih@gmail.com>
2019-02-16 01:13:05 +0100

    actions code styling

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,36 +8,42 @@ export default {
     commit(constants.TOGGLE_LOADING);
   },
   fetchRecentPosts({ commit }) {
-    return axios.get('/posts?get=10').then((res) => {
-      commit('SET_RECENT_POSTS', res.data.data);
+    return axios.get('/posts?get=10')
+      .then((res) => {
+        commit('SET_RECENT_POSTS', res.data.data);
 
-      return res.data;
-    });
+        return res.data;
+      });
   },
   fetchFeaturedPosts({ commit }) {
-    return axios.get('/posts?get=3&is_featured=1').then((res) => {
-      commit('SET_FEATURED', res.data.data);
+    return axios.get('/posts?get=3&is_featured=1')
+      .then((res) => {
+        commit('SET_FEATURED', res.data.data);
 
-      return res.data;
-    });
+        return res.data;
+      });
   },
   fetchAllPosts({ commit }) {
-    return axios.get('/posts?get=100').then((res) => {
-      commit('SET_ALL_POSTS', res.data.data);
+    return axios.get('/posts?get=100')
+      .then((res) => {
+        commit('SET_ALL_POSTS', res.data.data);
 
-      return res.data;
-    });
+        return res.data;
+      });
   },
   fetchBySlug(_, slug) {
-    return axios.get(`/posts/${slug}`).then(res => res.data);
+    return axios.get(`/posts/${slug}`)
+      .then(res => res.data);
   },
   search(_, params) {
-    return axios.get('/search', { params }).then(res => res.data);
+    return axios.get('/search', { params })
+      .then(res => res.data);
   },
   fetchTags({ commit }) {
-    return axios.get('/tags').then((res) => {
-      commit('SET_TAGS', res.data.data);
-    });
+    return axios.get('/tags')
+      .then((res) => {
+        commit('SET_TAGS', res.data.data);
+      });
   },
   savePost(_, data) {
     return axios.post('/posts', data);
