a8777c0550e600543e5fbaa1f058f3db080dbcad
İlker Demir <ilker46580@gmail.com>
2019-05-27 13:19:16 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,15 +3,25 @@ export const defaultEditorToolbar = [
     header: [false, 1, 2, 3, 4, 5, 6],
   }],
   ['bold', 'italic', 'underline', 'strike'],
-  [{ color: [] }, { background: [] }],
+  [
+    { color: [] }, 
+    { background: [] }
+  ],
   [
     { align: '' },
     { align: 'center' },
     { align: 'right' },
     { align: 'justify' },
   ],
-  [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
-  [{ indent: '-1' }, { indent: '+1' }],
+  [
+    { list: 'ordered' }, 
+    { list: 'bullet' },
+    { list: 'check' }
+  ],
+  [
+    { indent: '-1' }, 
+    { indent: '+1' }
+  ],
   ['link', 'video'],
   ['blockquote', 'code-block'],
   ['clean'],
