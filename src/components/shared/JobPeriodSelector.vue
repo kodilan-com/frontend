1f7cd64ceacc1bf1d794865a5b4511cc59a0ed38
Erdoğan Bulut <e-bulut@hotmail.com.tr>
2019-07-07 02:29:48 +0300

    Add 'tum ilanlar' button

--- src/components/shared/JobPeriodSelector.vue
+++ src/components/shared/JobPeriodSelector.vue
@@ -16,15 +16,51 @@ export default {
 </script>
 
 <template>
-  <ul class="tabs-nav">
-    <li
-      v-for="period in PERIODS"
-      :key="period.type"
-      :class="{ active: activePeriod === period.type }"
-    >
-      <router-link :to="`/${period.slug}`">
-        {{ period.text }}
-      </router-link>
-    </li>
-  </ul>
+  <section>
+    <ul class="tabs-nav">
+      <li
+        v-for="period in PERIODS"
+        :key="period.type"
+        :class="{ active: activePeriod === period.type }"
+      >
+        <router-link :to="`/${period.slug}`">
+          {{ period.text }}
+        </router-link>
+      </li>
+    </ul>
+    <div class="ul tabs-nav is-right">
+      <li>
+        <router-link to="/ilanlar">
+          Tüm İlanlar
+        </router-link>
+      </li>
+    </div>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+  section {
+    display: flex;
+    justify-content: space-between;
+    .tabs-nav {
+      width: auto;
+      @media (max-width: 767px) {
+        li {
+          line-height: 40px;
+          a {
+            padding: 0 12px;
+            font-size: 12px;
+          }
+        }
+      }
+      @media (max-width: 359px) {
+        li a {
+          padding: 0 10px;
+        }
+      }
+      &.is-right li a {
+        border-radius: 3px;
+      }
+    }
+  }
+</style>
