c48ea5606d10ca4872f0e634b3d98c12d2254ef0
obulat <obulat@gmail.com>
2019-07-07 08:54:25 +0300

    Close Subscribe component after user subscribed

--- src/components/shared/Subscribe.vue
+++ src/components/shared/Subscribe.vue
@@ -47,18 +47,27 @@ export default {
 
       const { frequency, name, email } = this;
 
-      this.subscribe({ frequency, name, email })
+      this.subscribe({
+        frequency,
+        name,
+        email,
+      })
         .then(() => {
           this.showDialog('Email listesine kaydınız gerçekleştirildi.');
+          this.close();
         })
         .catch((e) => {
-          const details = Object.values(e.response.data.errors || []).map(r => `<li>${r[0]}</li>`);
-
+          const details = Object.values(e.response.data.errors || [])
+            .map(r => `<li>${r[0]}</li>`);
           this.showDialog('Hata: Kaydınız gerçekleştirilemedi.', `<ul>${details || ''}</ul>`);
         });
     },
     showDialog(title, text, buttons = [{ title: 'Kapat' }]) {
-      this.$modal.show('dialog', { title, text, buttons });
+      this.$modal.show('dialog', {
+        title,
+        text,
+        buttons,
+      });
     },
     close() {
       this.isClosed = true;
@@ -74,39 +83,40 @@ export default {
     :class="{ fixed: fixed }"
     class="subscribe-widget"
   >
-    <span>Güncel iş ilanlarını</span>
-    <div class="subscribe--select">
-      <multiselect
-        v-model="frequencyModel"
-        :options="frequencyOptions"
-        label="text"
-        :searchable="false"
-        :close-on-select="true"
-      />
-    </div>
-    <span>olarak almak için</span>
-    <input type="text" v-model="name" placeholder="İsim soyisim" class="username">
-    <input type="text" v-model="email" placeholder="Email">
-    <button class="button" @click="handleSubscribe">
-      Abone ol!
-    </button>
-    <span
-      v-if="fixed"
-      class="close"
-      @click="close"
-    >
-      Bir daha gösterme :(
-    </span>
+    <form @submit.prevent="handleSubscribe">
+      <span>Güncel iş ilanlarını</span>
+      <div class="subscribe--select">
+        <multiselect
+          v-model="frequencyModel"
+          :options="frequencyOptions"
+          label="text"
+          :searchable="false"
+          :close-on-select="true"
+        />
+      </div>
+      <span>olarak almak için</span>
+      <input type="text" v-model="name" placeholder="İsim soyisim" class="username">
+      <input type="text" v-model="email" placeholder="Email">
+      <button class="button" @click="handleSubscribe">
+        Abone ol!
+      </button>
+      <span
+        v-if="fixed"
+        class="close"
+        @click="close"
+      >
+        Bir daha gösterme :(
+      </span>
+    </form>
   </div>
 </template>
 
 <style lang="scss">
-.subscribe-widget {
-  padding: 10px;
-  box-sizing: border-box;
-  font-size: 16px;
-  text-align: center;
-
+  .subscribe-widget {
+    padding: 10px;
+    box-sizing: border-box;
+    font-size: 16px;
+    text-align: center;
 
   .subscribe--select {
     text-align: left;
@@ -125,70 +135,83 @@ export default {
     border-top: 1px solid #474646;
     line-height: 50px;
 
+      input[type=text] {
+        margin-right: 6px;
+        width: 120px;
+        display: inline;
+
+        &.username {
+          margin-left: 6px;
+        }
+      }
+
+      select,
+      .subscribe--select {
+        display: inline-block;
+        width: 100px;
+        margin: 0 6px;
+        color: #202020;
+      }
+
+      .close {
+        display: block;
+        color: #5c5c5c;
+        font-size: 12px;
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        line-height: 12px;
+        cursor: pointer;
+
+        &:hover {
+          color: #ccc;
+        }
+      }
+    }
+
     input[type=text] {
-      margin-right: 6px;
-      width: 120px;
-      display: inline;
-
-      &.username {
-        margin-left: 6px;
-      }
+      padding: 11px 10px;
     }
 
-    select,
-    .subscribe--select {
-      display: inline-block;
-      width: 100px;
-      margin: 0 6px;
-      color: #202020;
+    select {
+      height: 42px;
     }
 
-    .close {
-      display: block;
-      color: #5c5c5c;
-      font-size: 12px;
-      position: absolute;
-      top: 10px;
-      right: 10px;
-      line-height: 12px;
-      cursor: pointer;
-
+    button {
       &:hover {
-        color: #ccc;
+        background-color: #26ae61;
       }
     }
   }
 
-  input[type=text] {
-    padding: 11px 10px;
-  }
+  @media only screen and (max-width: 768px) {
+    #footer {
+      padding-bottom: 10px !important;
+    }
 
-  select {
-    height: 42px;
-  }
-
-  button {
-    &:hover {
-      background-color: #26ae61;
+    .subscribe-widget.fixed {
+      display: none;
     }
   }
-}
 
-@media only screen and (max-width: 768px) {
-  #footer {
-    padding-bottom: 10px !important;
+  @media only screen and (max-width: 998px) {
+    .subscribe-widget.fixed {
+      height: 80px;
+    }
+    .subscribe-widget form {
+      margin-top: 10px;
+    }
+    .subscribe-widget.fixed form span.close {
+      top: 7px;
+    }
   }
 
-  .subscribe-widget.fixed {
-    display: none;
-  }
-}
+  @media only screen and (min-width: 990px) {
 
-@media only screen and (min-width: 990px) {
-  .subscribe-widget input[type=text] {
-    width: 140px !important;
+    .subscribe-widget input[type=text] {
+      width: 140px !important;
+    }
   }
-}
 
 @media only screen and (min-width: 1180px) {
   .subscribe-widget input[type=text] {
