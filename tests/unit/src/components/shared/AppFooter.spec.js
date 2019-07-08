b1ed3201c5efeb5c73470078582ace6f64971200
Mehmet Ersan Uzun <mersanuzun@gmail.com>
2019-07-08 16:30:11 +0300

    Move scrollY function to global helper

--- tests/unit/src/components/shared/AppFooter.spec.js
+++ tests/unit/src/components/shared/AppFooter.spec.js
@@ -2,6 +2,8 @@ import { shallowMount } from '@vue/test-utils';
 import AppFooter from '@/components/shared/AppFooter';
 
 describe('AppFooter.vue', () => {
+  const { helper } = global;
+
   afterEach(() => {
     jest.clearAllMocks();
   });
@@ -38,19 +40,12 @@ describe('AppFooter.vue', () => {
 
   describe('methods', () => {
     describe('handleScroll', () => {
-      const setScrollY = (value) => {
-        Object.defineProperty(window, 'scrollY', {
-          value,
-          writable: true,
-        });
-      };
-
       it('assign "isBackToTopVisible" to true, if scrollY is greater than 540', () => {
         const wrapper = shallowMount(AppFooter, {
           stubs: ['router-link'],
         });
 
-        setScrollY(541);
+        helper.scrollY(541);
 
         wrapper.vm.handleScroll();
 
@@ -62,7 +57,7 @@ describe('AppFooter.vue', () => {
           stubs: ['router-link'],
         });
 
-        setScrollY(539);
+        helper.scrollY(539);
 
         wrapper.vm.handleScroll();
 
@@ -74,7 +69,7 @@ describe('AppFooter.vue', () => {
           stubs: ['router-link'],
         });
 
-        setScrollY(540);
+        helper.scrollY(540);
 
         wrapper.vm.handleScroll();
 
@@ -86,27 +81,28 @@ describe('AppFooter.vue', () => {
   describe('templates', () => {
     let wrapper;
 
-    const generateWrapper = () => {
+    beforeEach(() => {
       wrapper = shallowMount(AppFooter, {
         stubs: ['router-link'],
       });
-    };
+    });
 
     afterEach(() => {
       wrapper.destroy();
     });
 
     it('should match the snapshot', () => {
-      generateWrapper();
-
       expect(wrapper.vm.$el).toMatchSnapshot();
     });
 
     it('should not render .has-widget div which its id is #footer, if subscribeClosed is true', () => {
       localStorage.getItem.mockReturnValue(true);
 
-      generateWrapper();
-      const footer = wrapper.find('#footer');
+      const localWrapper = shallowMount(AppFooter, {
+        stubs: ['router-link'],
+      });
+
+      const footer = localWrapper.find('#footer');
 
       expect(footer.classes('has-widget')).toBe(false);
     });
@@ -114,23 +110,21 @@ describe('AppFooter.vue', () => {
     it('should render .has-widget div which its id is #footer, if subscribeClosed is false', () => {
       localStorage.getItem.mockReturnValue(false);
 
-      generateWrapper();
-      const footer = wrapper.find('#footer');
+      const localWrapper = shallowMount(AppFooter, {
+        stubs: ['router-link'],
+      });
+      const footer = localWrapper.find('#footer');
 
       expect(footer.classes('has-widget')).toBe(true);
     });
 
     it('should show "backtotop" div, if isBackToTopVisible is true', () => {
-      generateWrapper();
-
       wrapper.setData({ isBackToTopVisible: true });
 
       expect(wrapper.find('#backtotop').exists()).toBe(true);
     });
 
     it('should not show "backtotop" div, if isBackToTopVisible is false', () => {
-      generateWrapper();
-
       wrapper.setData({ isBackToTopVisible: false });
 
       expect(wrapper.find('#backtotop').exists()).toBe(false);
--- tests/unit/src/setupTests.js
+++ tests/unit/src/setupTests.js
@@ -4,3 +4,16 @@ Object.defineProperty(window, 'localStorage', {
     writable: true,
   },
 });
+
+global.helper = {
+  scrollY(value) {
+    this.scroll('scrollY', value);
+  },
+
+  scroll(direction, value) {
+    Object.defineProperty(window, direction, {
+      value,
+      writable: true,
+    });
+  },
+};
