fd1604ea8fdded61e353bd8c93ea1e5cb6ebe5de
Fatih Acet <acetfatih@gmail.com>
2019-07-13 15:04:03 +0200

    Outdated Job details

--- src/components/pages/JobDetails.vue
+++ src/components/pages/JobDetails.vue
@@ -115,149 +115,156 @@ export default {
           Bu sadece bir önizlemedir. İlanınızı yayınlamak için lütfen kaydettiğinizden emin olun.
         </p>
       </div>
-      <template v-if="postData && postData.status == 1">
-        <div id="titlebar">
-          <div class="container">
-            <div class="ten columns">
-              <h1 itemprop="title">
-                {{ postData.position }}
-                <job-type-badge :post="postData" />
-              </h1>
-            </div>
-            <div class="six columns">
-              <a :href="twitterShareUrl" class="button dark" target="_blank">
-                <i class="fa fa-twitter" /> Paylaş
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="container detail-content-container">
-          <div class="eleven columns">
-            <div class="padding-right">
-              <div class="company-info"
-                   itemprop="hiringOrganization"
-                   itemscope
-                   itemtype="http://schema.org/Organization"
-              >
-                <router-link :to="`/firmalar/${postData.company.slug}`">
-                  <img
-                    itemprop="logo"
-                    :src="postData.company.logo"
-                    :alt="postData.company.name"
-                    ref="logo"
-                    @error="handleImageError"
-                  >
-                </router-link>
-                <div class="content">
-                  <h4 itemprop="name">
-                    {{ postData.company.name }}
-                  </h4>
-                  <span>
-                    <span itemprop="sameAs" :content="postData.company.www" />
-                    <a :href="postData.company.www" target="_blank">
-                      <i class="fa fa-link" /> Website
-                    </a>
-                  </span>
-                  <span v-if="postData.company.twitter">
-                    <a :href="twitterUrl" target="_blank">
-                      <i class="fa fa-twitter" /> @{{ postData.company.twitter.replace('@', '') }}
-                    </a>
-                  </span>
-                  <span v-if="postData.company.linkedin">
-                    <a :href="postData.company.linkedin" target="_blank">
-                      <i class="fa fa-linkedin" /> {{ postData.company.name }}
-                    </a>
-                  </span>
-                </div>
-                <div class="clearfix" />
+      <template v-if="postData">
+        <template v-if="postData.status !== 0">
+          <div id="titlebar">
+            <div class="container">
+              <div class="ten columns">
+                <h1 itemprop="title">
+                  {{ postData.position }}
+                  <job-type-badge :post="postData" />
+                </h1>
               </div>
-              <div class="job-description" itemprop="description" v-html="postData.description" />
+              <div class="six columns">
+                <a :href="twitterShareUrl" class="button dark" target="_blank">
+                  <i class="fa fa-twitter" /> Paylaş
+                </a>
+              </div>
             </div>
           </div>
-          <div class="five columns">
-            <div class="widget">
-              <div class="job-overview">
-                <ul>
-                  <li>
-                    <i class="fa fa-user" />
-                    <div>
-                      <strong>Pozisyon</strong>
-                      <span>{{ postData.position }}</span>
-                    </div>
-                  </li>
-                  <li>
-                    <i class="fa fa-map-marker" />
-                    <div>
-                      <strong>Lokasyon</strong>
-                      <span itemprop="jobLocation"
-                            itemscope
-                            itemtype="http://schema.org/Place"
+          <div class="container detail-content-container">
+            <template v-if="postData.status == 2">
+              <div class="notification notice">
+                <p>
+                  <span>Bu ilan yayından kaldırılmıştır.</span>
+                  <br><br>
+                  <span v-for="tag in postData.tags" :key="tag.slug" class="tag">
+                    <router-link :to="`/etiket/${tag.slug}`" class="listing-date">
+                      {{ tag.name }}
+                    </router-link>
+                  </span> etiketli diğer ilanlarımıza bakabilirsiniz.
+                </p>
+              </div>
+            </template>
+            <div :class="{ 'disabled-content': postData.status === 2 }">
+              <div class="eleven columns">
+                <div class="padding-right">
+                  <div class="company-info"
+                       itemprop="hiringOrganization"
+                       itemscope itemtype="http://schema.org/Organization"
+                  >
+                    <router-link :to="`/firmalar/${postData.company.slug}`">
+                      <img
+                        itemprop="logo"
+                        :src="postData.company.logo"
+                        :alt="postData.company.name"
+                        ref="logo"
+                        @error="handleImageError"
                       >
-                        <span itemprop="address"
-                              itemscope
-                              itemtype="http://schema.org/PostalAddress"
-                        >
-                          <span itemprop="addressLocality">
-                            {{ postData.location }}
-                          </span>
-                        </span>
-                      </span>
-                    </div>
-                  </li>
-                  <span itemprop="datePosted" :content="postData.updated_at" />
-                  <li>
-                    <i class="ln ln-icon-Globe" />
-                    <div>
-                      <strong>Website</strong>
+                    </router-link>
+                    <div class="content">
+                      <h4 itemprop="name">
+                        {{ postData.company.name }}
+                      </h4>
                       <span>
+                        <span itemprop="sameAs" :content="postData.company.www" />
                         <a :href="postData.company.www" target="_blank">
-                          {{ postData.company.www }}
+                          <i class="fa fa-link" /> Website
+                        </a>
+                      </span>
+                      <span v-if="postData.company.twitter">
+                        <a :href="twitterUrl" target="_blank">
+                          <i class="fa fa-twitter" />
+                          @{{ postData.company.twitter.replace('@', '') }}
+                        </a>
+                      </span>
+                      <span v-if="postData.company.linkedin">
+                        <a :href="postData.company.linkedin" target="_blank">
+                          <i class="fa fa-linkedin" /> {{ postData.company.name }}
                         </a>
                       </span>
                     </div>
-                  </li>
-                  <li>
-                    <i class="ln ln-icon-Tag-3" />
-                    <div>
-                      <p>
-                        <strong>Etiketler</strong>
-                      </p>
-                      <span
-                        v-for="tag in postData.tags"
-                        :key="tag.slug"
-                        class="tag"
-                      >
-                        <router-link :to="`/etiket/${tag.slug}`" class="listing-date">
-                          {{ tag.name }}
-                        </router-link>
-                      </span>
-                    </div>
-                  </li>
-                </ul>
-                <apply-link :post="postData" />
+                    <div class="clearfix" />
+                  </div>
+                  <div class="job-description"
+                       itemprop="description"
+                       v-html="postData.description"
+                  />
+                </div>
+              </div>
+              <div class="five columns">
+                <div class="widget">
+                  <div class="job-overview">
+                    <ul>
+                      <li>
+                        <i class="fa fa-user" />
+                        <div>
+                          <strong>Pozisyon</strong>
+                          <span>{{ postData.position }}</span>
+                        </div>
+                      </li>
+                      <li>
+                        <i class="fa fa-map-marker" />
+                        <div>
+                          <strong>Lokasyon</strong>
+                          <span itemprop="jobLocation"
+                                itemscope
+                                itemtype="http://schema.org/Place"
+                          >
+                            <span itemprop="address"
+                                  itemscope
+                                  itemtype="http://schema.org/PostalAddress"
+                            >
+                              <span itemprop="addressLocality">
+                                {{ postData.location }}
+                              </span>
+                            </span>
+                          </span>
+                        </div>
+                      </li>
+                      <span itemprop="datePosted" :content="postData.updated_at" />
+                      <li>
+                        <i class="ln ln-icon-Globe" />
+                        <div>
+                          <strong>Website</strong>
+                          <span>
+                            <a :href="postData.company.www" target="_blank">
+                              {{ postData.company.www }}
+                            </a>
+                          </span>
+                        </div>
+                      </li>
+                      <li>
+                        <i class="ln ln-icon-Tag-3" />
+                        <div>
+                          <p>
+                            <strong>Etiketler</strong>
+                          </p>
+                          <span
+                            v-for="tag in postData.tags"
+                            :key="tag.slug"
+                            class="tag"
+                          >
+                            <router-link :to="`/etiket/${tag.slug}`" class="listing-date">
+                              {{ tag.name }}
+                            </router-link>
+                          </span>
+                        </div>
+                      </li>
+                    </ul>
+                    <apply-link :post="postData" />
+                  </div>
+                </div>
               </div>
             </div>
+            <div class="sixteen columns" v-if="relatedPosts.length">
+              <h3 class="margin-bottom-25">
+                Benzer ilanlar
+              </h3>
+              <job-listing :posts="relatedPosts" />
+            </div>
           </div>
-          <div class="sixteen columns" v-if="relatedPosts.length">
-            <h3 class="margin-bottom-25">
-              Benzer ilanlar
-            </h3>
-            <job-listing :posts="relatedPosts" />
-          </div>
-        </div>
-      </template>
-      <template v-else-if="postData && postData.status == 2">
-        <div class="notification notice center">
-          <p>
-            <span>Bu ilan yayından kaldırılmış.</span>
-            <br>
-            Diğer <span v-for="tag in postData.tags" :key="tag.slug" class="tag">
-              <router-link :to="`/etiket/${tag.slug}`" class="listing-date">{
-                { tag.name }}
-              </router-link>
-            </span> ilanlarımızı incelemeye ne dersin?
-          </p>
-        </div>
+        </template>
       </template>
       <div v-else class="notification notice center">
         <p>
@@ -277,6 +284,11 @@ export default {
       font-size: inherit !important;
       color: inherit !important;
     }
+  }
+
+  .disabled-content {
+    pointer-events: none;
+    opacity: .5;
   }
 
   &.preview {
