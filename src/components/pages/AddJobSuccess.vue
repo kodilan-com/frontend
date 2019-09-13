53855a8eb2ce950cc38ab821d0b16227f00c6d80
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2019-09-13 08:47:23 +0300

    Create success page component and change success logic for new success component

---
+++ src/components/pages/AddJobSuccess.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: '',
+};
+</script>
+
+<template>
+  <div class="add-job">
+    <div
+      id="titlebar"
+      class="single submit-page"
+    >
+      <div class="container">
+        <div class="sixteen columns">
+          <h1>
+            <i class="fa fa-plus-circle" /> İlan Ekleme
+          </h1>
+        </div>
+      </div>
+    </div>
+    <div class="notification success center">
+      <p>
+        <span>İlanınız başarılı bir şekilde kaydedildi!</span><br><br>
+        İlanınızın yayınlanabilmesi için gönderilen e-postadaki
+        onay linkine tıklamanız gerekmektedir.
+      </p>
+    </div>
+  </div>
+</template>
