fe1b69efe03cb30709e804652f3910aea99667dd
İlker Kurtel <ilkerkurtel@gmail.com>
2019-09-16 13:16:46 +0300

    multiselect placeholder add

--- src/components/shared/Subscribe.vue
+++ src/components/shared/Subscribe.vue
@@ -86,6 +86,7 @@ export default {
         label="text"
         :searchable="false"
         :close-on-select="true"
+        placeholder="Seçim yapın"
       />
     </div>
     <span>olarak almak için</span>
