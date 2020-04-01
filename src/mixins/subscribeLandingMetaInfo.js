adca1aa160d2b072c519ad010c1ee1a1261947cc
serhatucek <serhatucek@gmail.com>
2020-04-01 05:09:02 +0300

    abone-ol için eksik meta bilgileri revizesi

---
+++ src/mixins/subscribeLandingMetaInfo.js
@@ -0,0 +1,17 @@
+export default {
+  metaInfo() {    
+    return {
+      title: 'Abone Ol, İş İlanlarını Kaçırma! - Kodilan.com',
+      meta: [
+        {
+          name: 'description',
+          content: 'E-Posta adresinize seçtiğiniz periyotlarda ücretsiz olarak güncel ilanları ulaştırbiliriz!',
+        },
+        {
+          name: 'keywords',
+          content: 'Frontend Developer ilan,Backend Developer ilan,Mobile Developer ilan, Devops Developer, istanbul developer ilan, izmir developer ilan, ankara developer ilan, Remote Developer ilan',
+        },
+      ],
+    };
+  },
+};
