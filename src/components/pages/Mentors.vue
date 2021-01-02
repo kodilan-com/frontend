af34c7a49db18e65aca99b49b387a8c273bdf427
Fatih Acet <acetfatih@gmail.com>
2021-01-02 03:09:26 +0100

    Mentors copy and SEO changes

--- src/components/pages/Mentors.vue
+++ src/components/pages/Mentors.vue
@@ -2,11 +2,46 @@
 import MentorCard from '../shared/MentorCard';
 
 const mentors = require('../../assets/data/mentors.json');
+const description = 'Alanında uzman kişilerden yazılım geliştirme, kariyer tavsiyeleri, tasarım, SEO ve DevOps gibi birçok konuda birebir randevu alabilirsiniz.';
+const pageTitle = 'Kodilan Mentorlar';
 
 export default {
   components: {
     MentorCard,
   },
+  metaInfo() {
+    return {
+      title: pageTitle,
+      meta: [
+        {
+          name: 'description',
+          content: description,
+        },
+        {
+          name: 'keywords',
+          content: 'mentör, yazılım kariyer tavsiyeleri, yazılım danışmanlık',
+        },
+        {
+          name: 'og:image',
+          content: 'https://kodilan.ams3.digitaloceanspaces.com/companies/kodilan.png',
+        },
+        {
+          name: 'og:description',
+          content: description,
+        },
+        {
+          name: 'og:title',
+          content: pageTitle,
+        },
+        {
+          name: 'og:url',
+          content: 'https://kodilan.com/mentorlar',
+        },
+      ],
+    };
+  },
+  description,
+  pageTitle,
   mentors: mentors.sort(() => Math.random() - 0.5),
 };
 </script>
@@ -16,9 +51,9 @@ export default {
     <div id="titlebar" class="single submit-page">
       <div class="container">
         <div class="sixteen columns">
-          <h1>Kodilan Mentorları</h1>
+          <h1>{{ $options.pageTitle }}</h1>
           <span class="headline">
-            Alanında uzman kişilerden yazılım geliştirme, kariyer tavsiyeleri, tasarım, SEO, DevOps gibi konularda danışmanlık almak için 1'e 1 randevu alabilirsiniz.
+            {{ $options.description }}
             <br>
             <br>
             Bu listede yer almak istiyorsanız, <a href="https://forms.gle/DYCQH1Wfj25TdHxH6" target="_blank" rel="noreferrer noopener">buradan</a> başvuruda bulunabilirsiniz.
